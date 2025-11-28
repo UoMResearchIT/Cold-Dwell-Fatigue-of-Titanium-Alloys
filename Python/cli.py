599a65aec50b9f77f6dd9c4b974e926a5a7de9b8
Martin HA <martin.herrerias@gmail.com>
2025-11-28 16:39:20 +0000

    CLI improvements (config file support, ...)
    - Load all defaults from defaults.yaml (remove dependency from config.py)
    - Remove explicit args check (will just be noisy)
    - Rename `template_path` and `runner_path` for consistency with env vars
    - Expand (and glob) `input_file`
    - Remove unused --version argument (should be reserved to print package version)
    - Add verbose option

--- Python/cli.py
+++ Python/cli.py
@@ -3,22 +3,20 @@ Command-line interface replacement for the old Tk GUI.
 """
 
 import os
-from warnings import warn
+from glob import glob
 import json
-from argparse import Namespace, ArgumentParser
+from configargparse import Namespace, ArgumentParser, YAMLConfigFileParser
 import subprocess
 
 from jinja2 import Environment, FileSystemLoader
 
-from config import Config
-
 
 def main():
     args = parse_args()
-    render_template(args.template_path, vars(args), args.json_path)
+    render_template(args.pipeline_template, vars(args), args.json_path)
 
-    if not args.no_run and args.runner_path:
-        run_pipeline(args.json_path, runner_path=args.runner_path)
+    if not args.no_run and args.pipeline_runner:
+        run_pipeline(args.json_path, runner_path=args.pipeline_runner)
 
 
 def render_template(template_name: str, context: dict, json_path: str) -> dict:
@@ -30,10 +28,6 @@ def render_template(template_name: str, context: dict, json_path: str) -> dict:
     )
     tpl = env.get_template(os.path.basename(template_name))
     rendered = tpl.render(**context)
-
-    # Debug: print rendered template
-    with open("debug_rendered_template.json", "w", encoding="utf8") as f:
-        f.write(rendered)
 
     # validate JSON before writing
     j = json.loads(rendered)
@@ -49,7 +43,9 @@ def run_pipeline(json_path: str, runner_path: str):
     """Run the DREAM3D PipelineRunner with the given JSON input file"""
 
     if not os.path.isfile(runner_path):
-        raise FileNotFoundError(f"PipelineRunner not found or invalid: {runner_path}")
+        raise FileNotFoundError(
+            f"PipelineRunner not found or invalid: {runner_path}"
+        )
     if not os.path.isfile(json_path):
         raise FileNotFoundError(f"JSON input file not found or invalid: {json_path}")
 
@@ -68,42 +64,60 @@ def run_pipeline(json_path: str, runner_path: str):
 
 def parse_args() -> Namespace:
 
-    cfg = Config()
+    def_config_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "defaults.yaml"
+    )
+    with open(def_config_file, "r") as f:
+        cfg = YAMLConfigFileParser().parse(f)
 
-    p = ArgumentParser(description="CLI for executing Dream3D pipeline templates")
-    p.add_argument("-f", "--input-file", help="Path to a single .ang or .ctf file")
+    p = ArgumentParser(
+        description="CLI for executing Dream3D pipeline templates",
+        config_file_parser_class=YAMLConfigFileParser,
+        default_config_files=["./.microtexture", "~/.microtexture"],
+    )
+
+    p.add_argument(
+        "-c",
+        "--config",
+        is_config_file=True,
+        help=f"Config file path. See {def_config_file} for an example.",
+    )
+    p.add_argument(
+        "-f", "--input-file", help="Path to a single .ang or .ctf file (required)"
+    )
     p.add_argument(
         "-o",
         "--output-dir",
-        default=r"Results/{basename}",
+        default=cfg["output_dir"],
         help="Results (sub)directory ['%(default)s']. {basename} will be replaced by "
-             "the input file name without extension.",
+        "the input file name without extension.",
     )
+    p.add_argument("-v", "--verbose", action="store_true")
 
     ang = p.add_argument_group("cleanup parameters for .ang files")
     ang._extension = "ang"  # see check_explicit_args
     ang.add_argument(
         "--ci-mask-threshold",
         type=float,
-        default=0.05,
+        default=cfg["ci_mask_threshold"],
         help="Confidence Index (CI) Threshold for Good Data [%(default)s]",
     )
     ang.add_argument(
         "--iq-mask-threshold",
         type=float,
-        default=20000.0,
+        default=cfg["iq_mask_threshold"],
         help="Image Quality (IQ) Threshold for Good Data [%(default)s]",
     )
     ang.add_argument(
         "--ci-primary-threshold",
         type=float,
-        default=0.05,
+        default=cfg["ci_primary_threshold"],
         help="Primary Cleanup CI Threshold [%(default)s]",
     )
     ang.add_argument(
         "--ci-secondary-threshold",
         type=float,
-        default=0.1,
+        default=cfg["ci_secondary_threshold"],
         help="Secondary Cleanup CI Threshold [%(default)s]",
     )
 
@@ -112,19 +126,19 @@ def parse_args() -> Namespace:
     ctf.add_argument(
         "--error-mask-threshold",
         type=int,
-        default=1,
+        default=cfg["error_mask_threshold"],
         help="Confidence Index (CI) Threshold for Good Data [%(default)s]",
     )
     ctf.add_argument(
         "--bc-primary-threshold",
         type=float,
-        default=30,
+        default=cfg["bc_primary_threshold"],
         help="Primary Cleanup Band Contrast (BC) Threshold [%(default)s]",
     )
     ctf.add_argument(
         "--bc-secondary-threshold",
         type=float,
-        default=50,
+        default=cfg["bc_secondary_threshold"],
         help="Secondary Cleanup BC Threshold [%(default)s]",
     )
 
@@ -132,48 +146,56 @@ def parse_args() -> Namespace:
     p.add_argument(
         "--caxis-misalignment",
         type=int,
-        default=20,
+        default=cfg["caxis_misalignment"],
         help="C-Axis Misalignment Threshold (deg) for Pixel Segmentation [%(default)s]",
     )
     p.add_argument(
         "--min-mtr-size",
         type=float,
-        default=10000,
+        default=cfg["min_mtr_size"],
         help="Minimum MTR Size, um^2 [%(default)s]",
     )
     p.add_argument(
         "--stress-axis",
         choices=["100", "010", "001"],
-        default="001",
+        default=cfg["stress_axis"],
         help="Stress axis direction (x='100', y='010', z='001') ['%(default)s']",
     )
 
     d3d = p.add_argument_group("DREAM3D execution")
     d3d.add_argument(
-        "--version", default=cfg.dream3d_version, help="DREAM3D version ['%(default)s']"
-    )
-    d3d.add_argument(
-        "--template-path",
-        default=cfg._dream3d_pipeline_template,
+        "--pipeline-template",
+        default=os.getenv("DREAM3D_PIPELINE_TEMPLATE", cfg["pipeline_template"]),
         help="Path to DREAM3D pipeline template ['%(default)s']. "
-             "{EXT} and {ext} tokens will be replaced by the (upper / lower case) "
-             "input file extension.",
+        "{EXT} and {ext} tokens will be replaced by the (upper / lower case) "
+        "input file extension. "
+        "Override default by setting DREAM3D_PIPELINE_TEMPLATE.",
     )
     d3d.add_argument(
+        "-n",
         "--no-run",
         action="store_true",
         help="Do not attempt to run PipelineRunner or analysis, just generate JSON file.",
     )
     d3d.add_argument(
-        "--runner-path",
-        default=cfg.dream3d_pipeline_runner,
-        help="Path to DREAM3D PipelineRunner [%(default)s]",
+        "--pipeline-runner",
+        default=os.getenv("DREAM3D_PIPELINE_RUNNER", cfg["pipeline_runner"]),
+        help="Path to DREAM3D PipelineRunner [%(default)s]. "
+        "Override default by setting DREAM3D_PIPELINE_RUNNER.",
     )
 
     args = p.parse_args()
 
+    args.input_file = os.path.expanduser(os.path.expandvars(args.input_file))
     if not os.path.isfile(args.input_file):
-        raise FileNotFoundError(f"Input file {args.input_file} does not exist.")
+        candidates = glob(args.input_file)
+        if len(candidates) > 1:
+            raise ValueError("Cannot yet handle multiple files")
+        elif len(candidates) == 0:
+            raise FileNotFoundError(f"Input file {args.input_file} does not exist.")
+        else:
+            args.input_file = candidates[0]
+    args.input_file = os.path.abspath(args.input_file)
 
     ext = os.path.splitext(args.input_file)[1].lower().strip(".")
     args.extension = ext
@@ -181,73 +203,33 @@ def parse_args() -> Namespace:
     if ext not in ["ang", "ctf"]:
         raise ValueError(f"Input file must be .ang or .ctf; got .{ext}.")
 
-    _check_explicit_args(p, ext)
-
-    args.template_path = args.template_path.format(EXT=ext.upper(), ext=ext.lower())
-    if not os.path.isfile(args.template_path):
-        raise FileNotFoundError(f"Template file {args.template_path} does not exist.")
+    args.pipeline_template = args.pipeline_template.format(
+        EXT=ext.upper(), ext=ext.lower()
+    )
+    if not os.path.isfile(args.pipeline_template):
+        raise FileNotFoundError(
+            f"Template file {args.pipeline_template} does not exist."
+        )
 
     basename = os.path.basename(args.input_file).rsplit(".", 1)[0]
     args.basename = basename
 
     args.output_dir = args.output_dir.format(basename=basename)
+    args.output_dir = os.path.abspath(
+        os.path.expanduser(os.path.expandvars(args.output_dir))
+    )
     args.json_path = os.path.join(args.output_dir, basename + ".json")
 
-    if not args.no_run and not os.path.isfile(args.runner_path):
+    if not args.no_run and not os.path.isfile(args.pipeline_runner):
         raise FileNotFoundError(
-            f"DREAM3D PipelineRunner not found at: {args.runner_path}"
+            f"DREAM3D PipelineRunner not found at: {args.pipeline_runner}"
         )
 
+    if args.verbose:
+        print("Parsed Inputs:")
+        [print(f"\t{k}: {v}") for k, v in vars(args).items()]
+
     return args
-
-
-def _check_explicit_args(
-    parser: ArgumentParser,
-    ext: str,
-):
-    """
-    Warn about explicit arguments that don't match the input file extension
-
-    Arguments that are specific to one file type are expected to be grouped
-    in an ArgumentGroup with an '_extension' attribute
-    """
-
-    # Get a dict {ext: set(arguments)} for argument groups tagged with an _extension
-    groups = {
-        g._extension: {a.dest for a in g._group_actions}
-        for g in parser._action_groups
-        if getattr(g, "_extension", None)
-    }
-
-    explicit = _explicit_args(parser)
-    own = groups.get(ext, set())
-
-    for e, v in groups.items():
-        if e == ext:
-            continue
-        weird = [a for a in v if getattr(explicit, a) and a not in own]
-        if any(weird):
-            warn(f"{e}-specific arguments provided but input file is .{ext}; ignoring.")
-
-
-def _explicit_args(parser: ArgumentParser) -> Namespace:
-    """\
-    Check which arguments were explicitly provided
-    Source: Elliot Way @ https://stackoverflow.com/a/69229790
-    """
-
-    class _Sentinel:
-        pass
-
-    names = {a.dest for a in parser._actions}
-
-    sentinel = _Sentinel()
-    sentinel_ns = Namespace(**{k: sentinel for k in names})
-    parser.parse_args(namespace=sentinel_ns)
-    explicit = Namespace(
-        **{k: (v is not sentinel) for k, v in vars(sentinel_ns).items()}
-    )
-    return explicit
 
 
 if __name__ == "__main__":
